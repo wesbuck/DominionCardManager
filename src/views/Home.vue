fbb23ffaebe3d74ad53aa0f57cf97d6c1a64b515
Wes Buck <wesbuck@gmail.com>
2019-12-22 16:40:46 -0500

    removed HelloWorld component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,13 @@
 <template>
   <div class="home">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p>Welcome to the app</p>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'home',
-  components: {
-    HelloWorld,
-  },
 }
 </script>
