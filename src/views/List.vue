74961a5e873df0065dd32b081e253a1b69fc6217
Wes Buck <wesbuck@gmail.com>
2019-12-22 20:15:31 -0500

    added Set filter

--- src/views/List.vue
+++ src/views/List.vue
@@ -1,24 +1,35 @@
 <template>
-  <div class="card-group">
-    <Card 
-      v-for="card in cards"
-      v-bind:key="card.card_name"
-      v-bind:card="card" 
-      @replace-card="replaceCard" 
-    />
+  <div class="row">
+    <div class="col-sm-2">
+      <h4>Choose Set:</h4>
+      <RadioFilter
+        v-bind:filters="allsets"
+        v-bind:name="'allsets'"
+        @update-filter="updateFilter" 
+        @remove-filter="fetchAllCards" 
+      />
+    </div>
+    <div class="card-group col-sm-10">
+      <Card 
+        v-for="card in cards"
+        v-bind:key="card.card_name"
+        v-bind:card="card"
+      />
+    </div>
   </div>
 </template>
 
 <script>
 import axios from "axios";
 import Card from "@/components/Card.vue";
+import RadioFilter from "@/components/RadioFilter.vue";
 import Header from "@/components/Header.vue";
 
 export default {
-  name: "CardSet",
-  props: "cards",
+  name: "List",
   components: {
     Card,
+    RadioFilter,
     Header,
   },
   data() {
@@ -30,20 +41,27 @@ export default {
   created() {
     axios
       .get("https://api.gameofboards.com/cards/")
-      .then(res => this.getCards(res))
+      .then(res => this.setupCards(res))
       .catch(err => console.log(err));
-
   },
   methods: {
     getCards(res) {
       this.cards = res.data;
+    },
+    setupCards(res) {
+      this.getCards(res);
       this.allsets = [...new Set(res.data.map(card => card.set_name))];
     },
-    replaceCard(name) {
-      var index = this.cards.findIndex(f => f.card_name === name)
+    updateFilter(name) {
       axios
-        .get("https://api.gameofboards.com/random/")
-        .then(res => (this.cards.splice(index, 1, res.data)))
+        .get("https://api.gameofboards.com/cards/?set_name="+name)
+        .then(res => this.getCards(res))
+        .catch(err => console.log(err));
+    },
+    fetchAllCards() {
+      axios
+        .get("https://api.gameofboards.com/cards/")
+        .then(res => this.getCards(res))
         .catch(err => console.log(err));
     },
   },
