3173d145df2d4c860840e1f966fd3d7d2c40ba58
Wes Buck <wes.buck@vidyard.com>
2019-12-23 12:23:14 -0500

    added Cost and Type filters

--- src/views/List.vue
+++ src/views/List.vue
@@ -1,13 +1,31 @@
 <template>
   <div class="row">
     <div class="col-sm-2">
+
       <h4>Choose Set:</h4>
       <RadioFilter
         v-bind:filters="allsets"
         v-bind:name="'allsets'"
-        @update-filter="updateFilter"
-        @remove-filter="fetchAllCards"
+        @update-filter="updateSetFilter"
+        @remove-filter="removeSetFilter"
       />
+
+      <h4>Choose Cost:</h4>
+      <RadioFilter
+        v-bind:filters="allcosts"
+        v-bind:name="'allcosts'"
+        @update-filter="updateCostFilter"
+        @remove-filter="removeCostFilter"
+      />
+
+      <h4>Choose Type:</h4>
+      <RadioFilter
+        v-bind:filters="alltypes"
+        v-bind:name="'alltypes'"
+        @update-filter="updateTypeFilter"
+        @remove-filter="removeTypeFilter"
+      />
+
     </div>
     <div class="card-group col-sm-10">
       <Card
@@ -36,6 +54,11 @@ export default {
     return {
       cards: [],
       allsets: [],
+      allcosts: [],
+      alltypes: [],
+      set_name: "",
+      cost: "",
+      type: "",
     };
   },
   created () {
@@ -50,18 +73,38 @@ export default {
     },
     setupCards (res) {
       this.getCards(res);
-      this.allsets = [...new Set(res.data.map(card => card.set_name))];
+      this.allsets = [...new Set(res.data.map(card => card.set_name))].sort();
+      this.allcosts = [...new Set(res.data.map(card => card.cost))].sort(function(a, b){return a.replace( /\D/g, '') - b.replace( /\D/g, '')}); //strip non-numeric & sort
+      this.alltypes = [...new Set(res.data.map(card => card.type))].sort();
     },
-    updateFilter (name) {
-      axios
-        .get(this.$endpointURL+"/cards/?set_name=" + name)
-        .then(res => this.getCards(res))
-        .catch(err => console.log(err));
+    updateSetFilter (name) {
+      this.set_name = name;
+      this.updateFilter();
     },
-    fetchAllCards () {
+    removeSetFilter () {
+      this.set_name = "";
+      this.updateFilter();
+    },
+    updateCostFilter (cost) {
+      this.cost = cost;
+      this.updateFilter();
+    },
+    removeCostFilter () {
+      this.cost = "";
+      this.updateFilter();
+    },
+    updateTypeFilter (type) {
+      this.type = type;
+      this.updateFilter();
+    },
+    removeTypeFilter () {
+      this.type = "";
+      this.updateFilter();
+    },
+    updateFilter () {
       axios
-        .get(this.$endpointURL+"/cards/")
-        .then(res => this.getCards(res))
+        .get(this.$endpointURL + "/cards/?set_name=" + this.set_name + "&cost=" + this.cost + "&type=" + this.type)
+        .then(res => this.setupCards(res))
         .catch(err => console.log(err));
     },
   },
