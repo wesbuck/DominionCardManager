270611c405dfe35dac4a58478ec989b0d35cbfa4
Wes Buck <wes.buck@vidyard.com>
2019-12-25 19:45:42 -0500

    implemented search functionality

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,8 +1,6 @@
 <template>
   <div>
-    <SearchBar
-      @search-cards="searchCards"
-     />
+    <SearchBar @search-cards="searchCards" />
     <div class="card-group">
       <Card
         v-for="card in cards"
@@ -22,7 +20,7 @@ import Header from "@/components/Header.vue";
 export default {
   name: "List",
   components: {
-    Card,    
+    Card,
     SearchBar,
     Header,
   },
@@ -47,8 +45,45 @@ export default {
       this.getCards(res);
     },
     searchCards (value, field) {
-      //TODO: implement search (make cards a subset of all_cards)
-      console.log("Search for '"+value+"' in '"+field+"' field(s)");
+      if ('' == value) {
+        this.cards = this.all_cards;
+        return;
+      }
+      var i = 0;
+
+      //search for 'value' in all fields
+      if ('All' == field) {
+        this.cards = [];
+        for (i = 0; i < this.all_cards.length; i++) {
+          var text = Object.values(this.all_cards[i]).join(' ');
+          if (text.toUpperCase().indexOf(value.toUpperCase()) > -1) {
+            this.cards.push(this.all_cards[i]);
+          }
+        }
+        return;
+      }
+
+      //search for 'value' in the selected 'field'
+      var key = "card_name";
+      switch (field) {
+        case 'Text':
+          key = 'card_text';
+          break;
+        case 'Cost':
+          key = 'cost';
+          break;
+        case 'Type':
+          key = 'type';
+          break;
+      }
+      console.log("Search for '" + value + "' in the '" + key + "' field");
+      this.cards = [];
+      for (i = 0; i < this.all_cards.length; i++) {
+        if (this.all_cards[i][key].toUpperCase().indexOf(value.toUpperCase()) > -1) {
+          this.cards.push(this.all_cards[i]);
+        }
+      }
+      return;
     },
   },
 };
@@ -56,6 +91,6 @@ export default {
 
 <style lang="scss" scoped>
 .form-control {
-  margin:0;
+  margin: 0;
 }
 </style>
